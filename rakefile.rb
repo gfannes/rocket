63bab887ecd5fc7a0efa5c74f8e401c8ebfb9c4d
Geert Fannes <geert.fannes@gmail.com>
2016-09-07 00:06:34 +0200

    added gubg.build and gubg.std

--- rakefile.rb
+++ rakefile.rb
@@ -1,19 +1,39 @@
-require(File.join(ENV['gubg'], 'shared'))
-require('gubg/build/Executable')
-include GUBG
+ENV['gubg'] = Dir.pwd
+require('./gubg.build/shared.rb')
 
-rocket = Build::Executable.new('rocket')
-rocket.add_define('DEBUG')
-rocket.add_include_path('src')
-rocket.add_include_path(shared_dir('include'))
-rocket.add_sources(FileList.new('src/**/*.cpp'))
-rocket.add_sources(FileList.new('src/**/*.hpp'))
-rocket.add_library_path(shared_dir('lib'))
-rocket.add_library('gubg.io')
+task :clean do
+    rm_rf('.cache')
+    rm(FileList.new("*.exe"))
+end
 
-task :build do
+def each_submod(&block)
+    submods = %w[build std].map{|n|"gubg.#{n}"}
+    GUBG::each_submod(submods, &block)
+end
+
+task :declare do
+    each_submod{sh 'rake declare'}
+end
+task :define => :declare do
+    each_submod{sh 'rake define'}
+end
+
+rocket = nil
+task :setup do
+    require('gubg/build/Executable')
+    rocket = Build::Executable.new('rocket')
+    rocket.add_define('DEBUG')
+    rocket.add_include_path('src')
+    rocket.add_include_path(GUBG::shared_dir('include'))
+    rocket.add_sources(FileList.new('src/**/*.cpp'))
+    rocket.add_sources(FileList.new('src/**/*.hpp'))
+    rocket.add_library_path(GUBG::shared_dir('lib'))
+end
+
+task :build => [:define, :setup] do
     rocket.build
 end
 task :run => :build do
     sh "./#{rocket.exe_filename}"
 end
+task :default => :run
